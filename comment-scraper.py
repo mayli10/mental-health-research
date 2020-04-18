365188a9678ef0545d16c9b6da9d2dcc43675a8f
May Li <mayli10x@gmail.com>
2020-04-18 16:01:04 -0400

    added new changes to comment scraper

--- comment-scraper.py
+++ comment-scraper.py
@@ -5,15 +5,16 @@ import time
 import datetime
 import pickle
 
-old_post_ids = []
-commentCount = 0
-commentStats = {}
-
+# get post ids
 with open ('./post-ids.p', 'rb') as fp:
-    old_post_ids = pickle.load(fp)
+    post_ids = pickle.load(fp)
 
-post_ids = old_post_ids[old_post_ids.index("acvl9n") + 1:]
+# get current post ids that have already been turned into a comment file
+with open ('./post_comment_file_ids.p', 'rb') as fp:
+    post_comment_file_ids = pickle.load(fp)
+
 print(len(post_ids))
+print(len(post_comment_file_ids))
 
 def getPushshiftComments(id):
     url = 'https://api.pushshift.io/reddit/comment/search/?link_id='+str(id)+'&limit=20000'
@@ -30,29 +31,61 @@ def collectCommentData(comment):
     score = comment['score']
     created = datetime.datetime.fromtimestamp(comment['created_utc']) #1520561700.0
     permalink = comment['permalink']
-    text = comment['body']
+    text = " ".join(comment['body'].split("\n")) #get rid of \n new line
     stickied = comment['stickied']
     parent_id = comment['parent_id']
     link_id = comment['link_id']
     is_submitter = comment["is_submitter"]
 
     commentData.append((comment_id,link_id,parent_id,author,score,created,permalink,text, stickied,is_submitter))
-    commentStats[comment_id] = commentData
+    commentStats[first_id][comment_id] = commentData
 
 def create_comments_file(id):
-    file = "./comments-csv/comments-csv/comment-" + str(id) + ".csv"
+    file = "./comments/comment-" + str(id) + ".csv"
     with open(file, 'w', newline='', encoding='utf-8') as file:
         a = csv.writer(file, delimiter=',')
         headers = ["Comment ID","Link ID","Parent ID","Author","Score","Publish Date","Permalink","Text", "Stickied", "Is Submitter"]
         a.writerow(headers)
-        for comment in commentStats:
-            a.writerow(commentStats[comment][0])
+        dict_values = commentStats[id].values()
+        for comment in dict_values:
+            fields = []
+            for f in comment:
+                fields.extend(f)
+            a.writerow(fields)
 
-for id in post_ids:
-    data = getPushshiftComments(id)
-    for comment in data:
-        collectCommentData(comment)
-        create_comments_file(id)
+# test for one file
+first_id = post_ids[0]
+commentStats = { first_id : {} }
+
+data = getPushshiftComments(first_id)
+
+for comment in data:
+    collectCommentData(comment)
+create_comments_file(first_id)
+post_comment_file_ids.append(first_id)
+
+#######################################################
+# new correct way of getting all comments for each post
+#######################################################
+# for id in post_ids:
+#     data = getPushshiftComments(id)
+#     commentStats[id] = {}
+#     for comment in data:
+#         collectCommentData(comment)
+#     create_comments_file(first_id)
+
+#######################################################
+# old wrong way of getting all comments for each post:
+#######################################################
+# for id in post_ids:
+#     data = getPushshiftComments(id)
+#     for comment in data:
+#         collectCommentData(comment)
+#         create_comments_file(id)
+
+# pickle the comment ids again
+with open('./post_comment_file_ids.p', 'wb') as fp:
+    pickle.dump(post_comment_file_ids, fp)
 
 # print(str(len(commentStats)) + " comments have added to list")
 # print("1st entry is:")
