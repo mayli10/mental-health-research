98b2c842713f5af5f2249e271984c88bb8ba9ce7
May Li <mayli10x@gmail.com>
2020-04-20 10:09:50 -0400

    updated get-comment-file-ids script

--- get-comment-file-ids.py
+++ get-comment-file-ids.py
@@ -1,6 +1,12 @@
 import os
-def get_ids:       # 1.Get file names from directory
+def get_ids():       # 1.Get file names from directory
     file_list=os.listdir(r"/home/mlz387/mental-health-research/comments")
-    print(file_list)
+    new_file_list = []
+    for file in file_list:
+        split_file = file.split('-')
+        new_file_list.append(split_file[1])
     
+    print(new_file_list)
+
+get_ids()
 
